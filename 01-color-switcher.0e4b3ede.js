2fab8de844e97aaf4c96ad6d34648c2dcab6c7be
Stanislav-Zhuk <114289905+Stanislav-Zhuk@users.noreply.github.com>
2023-03-19 08:12:22 +0000

    Deploying to gh-pages from @ Stanislav-Zhuk/goit-js-hw-09@d03a5fc9ba1ab58a602ced4bab7913754d43bd1a 🚀

---
+++ 01-color-switcher.0e4b3ede.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!1,t.disabled=!0})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.0e4b3ede.js.map
